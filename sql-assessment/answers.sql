e8539b0d7312aa6ae37c4413a8a82d8f89a47fff
moomoo123456 <90441229+moomoo123456@users.noreply.github.com>
2023-09-22 17:33:28 -0500

    Update answers.sql

--- sql-assessment/answers.sql
+++ sql-assessment/answers.sql
@@ -1,44 +1,49 @@
+-- 1. Get the sum of impressions by day.
 SELECT date, SUM(impressions) AS total_impressions
-FROM marketing_performance
-GROUP BY date
-ORDER BY date;
+FROM marketing_data
+GROUP BY date;
 
+-- 2. Get the top three revenue-generating states in order of best to worst. How much revenue did the third best state generate?
 SELECT state, SUM(revenue) AS total_revenue
 FROM website_revenue
 GROUP BY state
 ORDER BY total_revenue DESC
 LIMIT 3;
 
-SELECT c.name AS campaign_name,
-       SUM(mp.cost) AS total_cost,
-       SUM(mp.impressions) AS total_impressions,
-       SUM(mp.clicks) AS total_clicks,
+-- 3. Show total cost, impressions, clicks, and revenue of each campaign, including campaign name.
+SELECT ci.name AS campaign_name,
+       SUM(md.cost) AS total_cost,
+       SUM(md.impressions) AS total_impressions,
+       SUM(md.clicks) AS total_clicks,
        SUM(wr.revenue) AS total_revenue
-FROM campaign_info c
-JOIN marketing_performance mp ON c.id = mp.campaign_id
-JOIN website_revenue wr ON c.id = wr.campaign_id
-GROUP BY c.name;
+FROM campaign_info ci
+JOIN marketing_data md ON ci.campaign_id = md.campaign_id
+JOIN website_revenue wr ON ci.campaign_id = wr.campaign_id
+GROUP BY ci.name;
 
-SELECT wr.state, SUM(mp.conversions) AS total_conversions
-FROM marketing_performance mp
-JOIN website_revenue wr ON mp.campaign_id = wr.campaign_id
-WHERE mp.campaign_id = 'Campaign5'
-GROUP BY wr.state
+-- 4. Get the number of conversions of Campaign5 by state. Which state generated the most conversions for this campaign?
+SELECT md.geo AS state,
+       SUM(md.conversions) AS total_conversions
+FROM marketing_data md
+WHERE md.campaign_id = 'Campaign5'
+GROUP BY md.geo
 ORDER BY total_conversions DESC
 LIMIT 1;
 
-SELECT c.name AS campaign_name,
-       SUM(wr.revenue) / SUM(mp.cost) AS efficiency
-FROM campaign_info c
-JOIN marketing_performance mp ON c.id = mp.campaign_id
-JOIN website_revenue wr ON c.id = wr.campaign_id
-GROUP BY c.name
+-- 5. Identify the most efficient campaign based on the revenue-to-cost ratio.
+SELECT ci.name AS campaign_name,
+       SUM(wr.revenue) / SUM(md.cost) AS efficiency
+FROM campaign_info ci
+JOIN marketing_data md ON ci.campaign_id = md.campaign_id
+JOIN website_revenue wr ON ci.campaign_id = wr.campaign_id
+GROUP BY ci.name
 ORDER BY efficiency DESC
 LIMIT 1;
 
+-- Bonus: Find the best day of the week to run ads based on average impressions.
 SELECT DATE_FORMAT(date, '%W') AS day_of_week,
        AVG(impressions) AS avg_impressions
-FROM marketing_performance
+FROM marketing_data
 GROUP BY day_of_week
 ORDER BY avg_impressions DESC
 LIMIT 1;
